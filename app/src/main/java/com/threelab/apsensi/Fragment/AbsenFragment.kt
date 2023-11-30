9b527ef6a9bd181c9ed35c8cdbe1678a866332f2
irsyadulibad <ahmadirsyadulibad7@gmail.com>
2023-11-30 14:22:22 +0700

    fix: attendance camera

--- app/src/main/java/com/threelab/apsensi/Fragment/AbsenFragment.kt
+++ app/src/main/java/com/threelab/apsensi/Fragment/AbsenFragment.kt
@@ -1,16 +1,41 @@
 package com.threelab.apsensi.Fragment
 
+import android.Manifest
+import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Base64
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.Volley
+import com.threelab.apsensi.Helper.Constant
+import com.threelab.apsensi.Helper.ImageUploader
+import com.threelab.apsensi.Helper.PreferencesHelper
 import com.threelab.apsensi.R
-import com.threelab.apsensi.ScanActivity2
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
 
 class AbsenFragment  : Fragment() {
+    private var urlImage: Bitmap? = null
+    private var photoFile: File? = null
+
+    private lateinit var requestQueue: RequestQueue
+    private lateinit var photoText: TextView
+    private lateinit var sharedPref: PreferencesHelper
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -19,11 +44,86 @@ class AbsenFragment  : Fragment() {
         val view = inflater.inflate(R.layout.fragment_absen, container, false)
 
         val cameraScan: Button = view.findViewById(R.id.cameraScan)
+        photoText = view.findViewById(R.id.photoText)
+        requestQueue = Volley.newRequestQueue(requireContext())
+        sharedPref = PreferencesHelper(requireContext())
 
         cameraScan.setOnClickListener {
-            val intent = Intent(activity, ScanActivity2::class.java)
-            startActivity(intent)
+            capturePhoto()
         }
+
+        initializedOpenCamera()
+
         return view
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        val endpoint = Constant.API_ENDPOINT + "/attendances/attempt"
+        val authToken = sharedPref.getString(Constant.PREF_TOKEN) ?: ""
+
+
+        if (resultCode == Activity.RESULT_OK && requestCode == 200 && data != null) {
+            urlImage = data.extras?.get("data") as Bitmap?
+
+            ImageUploader(requireContext()).uploadImage(
+                endpoint,
+                authToken,
+                bitmapToByteArray(urlImage),
+                {response ->
+
+                },
+                {error ->
+                    Log.e("Anjay", String(error.networkResponse.data))
+                }
+            )
+        }
+    }
+
+    private fun saveBitmapToFile(bitmap: Bitmap?): File {
+        val file = File.createTempFile("temp_image", null, requireActivity().cacheDir)
+
+        try {
+            val outputStream = FileOutputStream(file)
+            bitmap?.compress(Bitmap.CompressFormat.PNG, 100, outputStream)
+            outputStream.flush()
+            outputStream.close()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+
+        return file
+    }
+
+    fun bitmapToByteArray(bitmap: Bitmap?): ByteArray {
+        val stream = ByteArrayOutputStream()
+        bitmap?.compress(Bitmap.CompressFormat.PNG, 100, stream)
+        return stream.toByteArray()
+    }
+
+    private fun capturePhoto() {
+        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        startActivityForResult(cameraIntent, 200)
+    }
+
+    private fun initializedOpenCamera() {
+        if (ContextCompat.checkSelfPermission(
+                requireContext(), Manifest.permission.CAMERA
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(
+                    requireActivity(), Manifest.permission.CAMERA
+                )
+            ) {
+                ActivityCompat.requestPermissions(
+                    requireActivity(), arrayOf(Manifest.permission.CAMERA), 1
+                )
+            } else {
+                ActivityCompat.requestPermissions(
+                    requireActivity(), arrayOf(Manifest.permission.CAMERA), 1
+                )
+            }
+        }
     }
 }
