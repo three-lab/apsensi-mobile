356f9af3e5c01ac82175fb1f2bf6698436c05804
irsyadulibad <ahmadirsyadulibad7@gmail.com>
2023-12-06 12:11:53 +0700

    add: otp verification

--- app/src/main/java/com/threelab/apsensi/OtpActivity.kt
+++ app/src/main/java/com/threelab/apsensi/OtpActivity.kt
@@ -3,74 +3,65 @@ package com.threelab.apsensi
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+import com.threelab.apsensi.Helper.Constant
+import org.json.JSONObject
 
 class OtpActivity : AppCompatActivity() {
 
     private lateinit var editTextOtp: EditText
+    private lateinit var requestQueue: RequestQueue
+    private lateinit var loadingDialog: LoadingDialog
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.dialog_otppass)
 
         editTextOtp = findViewById(R.id.edit_otp)
+        requestQueue = Volley.newRequestQueue(this@OtpActivity)
+        loadingDialog = LoadingDialog(this@OtpActivity)
+
         val btnVerifyOtp: Button = findViewById(R.id.btn_lanjut)
         val backlogin: TextView = findViewById(R.id.backlogin)
+        val username = intent?.getStringExtra("username") ?: ""
 
         backlogin.setOnClickListener {
-            val intent = Intent(this , MainActivity::class.java)
+            val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
             finish()
         }
 
-        // Generate random 6-digit OTP
-        val generatedOtp = generateOtp()
-
-        // Simpan OTP ke dalam shared preferences atau sesuai kebutuhan
-        saveOtpToSharedPreferences(generatedOtp)
-
-        // Tampilkan OTP pada EditText atau TextView
-        editTextOtp.setText(generatedOtp)
-
         btnVerifyOtp.setOnClickListener {
-            val userEnteredOtp = editTextOtp.text.toString()
-            val generatedOtp =
-                getStoredOtpFromSharedPreferences() // Gantilah dengan metode yang benar untuk mendapatkan OTP yang disimpan
-
-            if (userEnteredOtp == generatedOtp) {
-                showToast("OTP Verified Successfully")
-
-                // OTP benar, arahkan ke halaman DialogNewPassActivity
-//                val intent = Intent(this ,newpass::class.java)
-//                startActivity(intent)
-//                finish()
-            } else {
-                showToast("Incorrect OTP. Please try again.")
-            }
+            loadingDialog.showLoading()
+            verifyOtp(username, editTextOtp.text.toString())
         }
     }
 
-    private fun generateOtp(): String {
-        // Generate random 6-digit OTP
-        return (100000..999999).random().toString()
-    }
+    private fun verifyOtp(username: String, otp: String) {
+        val forgotUrl = Constant.API_ENDPOINT + "/forgot-pass"
+        val dataRequest = JSONObject();
 
-    private fun saveOtpToSharedPreferences(otp: String) {
-        // Simpan OTP ke dalam shared preferences atau sesuai kebutuhan
-        // Implementasi disesuaikan dengan kebutuhan aplikasi Anda
-    }
+        dataRequest.put("username", username)
+        dataRequest.put("code", otp)
 
-    private fun showToast(message: String) {
-        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
-    }
+        val forgotRequest = JsonObjectRequest(Request.Method.POST, forgotUrl, dataRequest,
+            { response ->
+                Toast.makeText(this, "Kode OTP Berhasil Diverifikasi", Toast.LENGTH_SHORT)
+                    .show()
+            },
+            { error ->
 
-    private fun getStoredOtpFromSharedPreferences(): String {
-        // Implementasi disesuaikan dengan kebutuhan aplikasi Anda
-        // Mengambil OTP yang telah disimpan dari SharedPreferences
-        return "123456" // Gantilah dengan metode yang benar untuk mendapatkan OTP yang disimpan
+            });
+
+        requestQueue.add(forgotRequest)
     }
 }
